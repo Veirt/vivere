3c7d13166936c8a5d3df7b92b6c9c3359bb1e044
Veirt <veirt@duck.com>
2024-04-06 00:27:16 +0800

    feat(web): better navbar and color

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,14 +1,14 @@
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import "./navbar.css";
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="flex gap-2 p-2">
-        <Link to="/" className="[&.active]:font-bold">
+      <div className="flex gap-2 p-4 text-white border-b navbar-bg">
+        <Link to="/" className="[&.active]:font-bold active-item">
           Home
         </Link>
       </div>
-      <hr />
       <Outlet />
     </>
   ),
